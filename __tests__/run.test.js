14f9490dd57782d74a13d5934a242a8710e63a46
rtn-scotty <scott@roadtripnation.com>
2018-08-03 13:00:20 -0700

    added cwd option

--- __tests__/run.test.js
+++ __tests__/run.test.js
@@ -1,13 +1,29 @@
 const {run} = require('../index');
 
-//test('run npm install', async () => {
-  //await run('npm i');
-  //expect(true).toBe(true);
-//});
-
-test('run npm install with callback', () => {
-  run("ls -al", () => {
-    expect(true).toBe(true);
-  });
+test('run npm install', async () => {
+  await run('npm i');
+  expect(true).toBe(true);
 });
+
+test('run ls with callback', (done) => {
+  const callback = () => {
+    expect(true).toBe(true);
+    done();
+  };
+  run("ls -al", callback);
+});
+
+test('run ls with callback', (done) => {
+  const callback = () => {
+    expect(true).toBe(true);
+    done();
+  };
+  run("ls -al", `${__dirname}/tmp`, callback);
+});
+
+//test('run ls with capture output', async (done) => {
+  //const lsOutput = await run("ls");
+  //expect(lsOutput.indexOf('run') > -1).toBe(true);
+  //expect(lsOutput.indexOf('run') > -1).toBe(true);
+//});
 
--- index.js
+++ index.js
@@ -1,35 +1,65 @@
 const spawn = require('child_process').spawn;
 
-const run = (command, callback) => {
+const run = (command, opt1, opt2, opt3) => {
   const options = command.split(' ');
   const type = options.shift();
 
-  const fullParentPath = module.parent.filename;
-  const pathParts = fullParentPath.split('/');
-  pathParts.pop();
-  const parentDirname = pathParts.join('/');
+  const opt1Type = typeof(opt1);
+  const opt2Type = typeof(opt2);
+  const opt3Type = typeof(opt3);
+  let cwd, capture, callback = false;
 
-  //console.log(parentDirname);
-
-  const spawnOptions = {
-    cwd: `${parentDirname}/`,
-    stdio: 'inherit'
-  };
-
-  const commandSpawn = spawn(type, options, spawnOptions);
-
-  if(callback) {
-    commandSpawn.on('close', (code) => {
-      console.log('callback');
-      return callback();
-    });
+  switch(opt1Type) {
+    case 'string':
+      cwd = opt1;
+      break;
+    case 'function':
+      callback = opt1;
+      break;
+    case 'bool':
+      capture = opt1;
+      break;
   }
 
-  return new Promise((resolve, reject) => {
-    commandSpawn.on('close', (code) => {
-      return resolve();
+  switch(opt2Type) {
+    case 'function':
+      callback = opt2;
+      break;
+    case 'bool':
+      capture = opt2;
+      break;
+  }
+
+  //set cwd if one is not given
+  if (!cwd) {
+    const fullParentPath = module.parent.filename;
+    const pathParts = fullParentPath.split('/');
+    pathParts.pop();
+    const parentDirname = pathParts.join('/');
+    cwd = `${parentDirname}/`;
+  }
+
+  const spawnOptions = {
+    cwd,
+    stdio: capture ? '' : 'inherit'
+  };
+
+  if (!capture) {
+
+    const commandSpawn = spawn(type, options, spawnOptions);
+
+    if(callback) {
+      commandSpawn.on('close', (code) => {
+        return callback();
+      });
+    }
+
+    return new Promise((resolve, reject) => {
+      commandSpawn.on('close', (code) => {
+        return resolve();
+      });
     });
-  });
+  }
 };
 
 module.exports = {
